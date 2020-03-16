bb124b0eba3ec0b336459743eb11e80be6942fbd
gbishton <graham.bishton@googlemail.com>
2020-03-16 21:29:10 +0000

    Add files via upload

---
+++ Models/SearchEngine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SearchInvestigation
+{
+    public class SearchEngine
+    {
+        public string Name { get; set; }
+        public bool Enabled { get; set; }
+
+        public Uri SearchURL { get; set; }
+
+        public string[] SearchResults { get; set; }
+    }
+}
---
+++ Models/SearchInvestigationViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SearchInvestigation
+{
+    public class SearchInvestigationViewModel
+    {
+        public List<SearchEngine> SearchEngines { get; set; }
+
+        [Required(ErrorMessage = "Please enter a Search Term")]
+        public string SearchTerm { get; set; }
+    }
+}
