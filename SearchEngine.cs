28149f3c5a19c827a8637a14c828754dee1482b0
gbishton <graham.bishton@googlemail.com>
2020-03-17 14:43:44 +0000

    Add files via upload

---
+++ SearchEngine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SearchInvestigation
+{
+    public class SearchEngine
+    {
+        public string Name { get; set; }
+        public bool Enabled { get; set; }
+
+        public Uri SearchURL { get; set; }
+
+        public List<Uri> SearchResults { get; set; }
+
+        public string Selector { get; set; }
+    }
+}
