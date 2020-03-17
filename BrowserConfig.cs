99c1f0eb6080df6e68d4b54ce79e07e133c3c15d
gbishton <graham.bishton@googlemail.com>
2020-03-17 14:42:58 +0000

    Add files via upload

---
+++ BrowserConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using PuppeteerSharp;
+
+
+namespace SearchInvestigation
+{
+    public class BrowserConfig
+    {
+        public static async Task DownloadChromeRevision()
+        {
+            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+        }
+    }
+}
